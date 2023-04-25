ec23ca25a7364c03544fe58c6fe062d207866d8d
Aubrey Gunn <agunn369@gmail.com>
2023-04-24 20:58:19 -0500

    update schema and seeds content

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,26 @@ CREATE DATABASE employeetracker_db;
 USE employeetracker_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR (30)
+    id INT NOT NULL,
+    name VARCHAR (30),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT NOT NULL,
     title VARCHAR (30),
     salary DECIMAL,
-    department_id INT
+    FOREIGN KEY (department_id) 
+    REFERENCES department(id) 
+    ON DELETE SET NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employees (
     id INT PRIMARY KEY,
     first_name VARCHAR (30),
     last_name VARCHAR (30),
-    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES 
     manager_id INT 
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,25 @@
 INSERT INTO department (id, name)
-VALUES  (001, "Mr.Smith"),
-        (002, "Mr.Bean"),
-        (003, "Mr.Man");
+VALUES  (001, "Engineering"),
+        (002, "Finance"),
+        (003, "Legal"),
+        (004, "Sales");
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES  (001, "manager", "35,000", "001"),
-        (002, "assistant manager", "34,000", "002"),
-        (003, "employee", "32,000", "003");
+VALUES  (001, "Sales Lead", 100000, 1),
+        (002, "Salesperson", 80000, 1),
+        (003, "Lead Engineer", 150000, 2)
+        (004, "Software Engineer", 120000, 2)
+        (005, "Account Manager", 160000, 3),
+        (006, "Accountant", 125000, 3),
+        (007, "Legal Team Lead", 250000, 4),
+        (008, "Lawyer", 190000, 4);
 
 INSERT INTO employees (id, first_name, last_name, manager_id)
-VALUES  (),
-        (),
-        ();
+VALUES  ("John", "Doe", null),
+        ("Mike", "Chan", "John Doe"),
+        ("Ashley", "Rodriguez", null)
+        ("Kevin", "Tupik", "Ashley Rodriguez"),
+        ("Kunal", "Singh", null),
+        ("Malia", "Brown", "Kunal Singh"),
+        ("Sarah", "Lourd", null),
+        ("Tom", "Allen", "Sarah Lourd");
