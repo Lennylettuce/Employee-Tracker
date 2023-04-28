f3e91a242cf4202a770eabb503571f3023653e62
Aubrey Gunn <agunn369@gmail.com>
2023-04-28 13:18:21 -0500

    added query functions for all db tasks

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
-
-const PORT = process.envv.PORT | 3001;
+const inquirer = require('inquirer');
+//const PORT = process.envv.PORT | 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -76,6 +76,7 @@ const choices = async (options) => {
 };
 
 //View all departments
+
 const viewDepartment = () => {
     const query = 'SELECT * FROM department';
     db.query(query, (err, department) => {
@@ -84,19 +85,180 @@ const viewDepartment = () => {
         start();
     });
 };
+
 //View all roles
+
+const viewRoles = () => {
+    const query = `SELECT * FROM role`;
+    connection.query(query, (err, role) => {
+      if (err) throw err;
+      console.table(role);
+      start();
+    });
+};
 
 //View all employees
 
+const viewEmployees = () => {
+    const query = `SELECT * FROM employees`;
+    connection.query(query, (err, employees) => {
+      if (err) throw err;
+      console.table(employees);
+      start();
+    });
+};
+
 // Add department
+
+const addDepartment = async () => {
+    try {
+        const newDepartment = await inquirer.prompt([
+            {
+             name: 'name',
+             type: 'input',
+             message: 'Type in name of new department'
+            }
+        ]);
+        db.query('INSERT INTO department(name) VALUES(?)', newDepartment.name);
+        console.log('New department added!');
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 // Add a role
 
+const addRoles = async () => {
+    try {
+        const { title, salary, department } = await inquirer.prompt([
+            {
+                name: 'title',
+                type: 'input',
+                message: 'Add title of new role',
+            },
+            {
+                name: 'salary',
+                type: 'number',
+                message: 'Add salary for new role',
+            },
+            {
+                name: 'department',
+                type: 'list',
+                message: 'What department does new role fit?',
+                choices: [
+                    {name: 'Engineering', value: 1 },
+                    {name: 'Finance', value: 2 },
+                    {name: 'Legal', value: 3 },
+                    {name: 'Sales', value: 4 }
+                ]
+            },
+        ]);
+        const query = 'INSERT INTO role SET ?';
+        db.query(query, { title, salary, department_id: department }, (err, title) => {
+            console.log('New role added!');
+            start();
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
 // Add employee
+
+const addEmployee = async () => {
+    try {
+        const { first, last, role, manager } = await inquirer.prompt([
+            {
+                name: 'first',
+                type: 'input',
+                message: 'Add employee first name',
+            },
+            {
+                name: 'last',
+                type: 'input',
+                message:'Add employee last name',
+            },
+            {
+                name: 'role',
+                type: 'list',
+                message: 'Add employee role',
+                choices: [
+                    { name: 'Sales Lead', value: 1 },
+                    { name: 'Salesperson', value: 2 },
+                    { name: 'Lead Engineer', value: 3 },
+                    { name: 'Software Engineer', value: 4 },
+                    { name: 'Account Manager', value: 5 },
+                    { name: 'Accountant', value: 6 },
+                    { name: 'Legal Team Lead', value: 7 },
+                    { name: 'Lawyer', value: 8 },
+                ]
+            },
+            {
+                name: 'manager',
+                type: 'list',
+                message: 'Add manger for employee',
+                choices: [
+                    { name: 'John Doe', value: 1 },
+                    { name: 'Ashley Rodriguez', value: 3 },
+                    { name: 'Kunal Singh', value: 5 },
+                    { name: 'Sarah Lourd', value: 7 },
+                    { name: 'none', value: null }
+                ]
+            }
+        ]);
+        const query = 'INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES(?, ?, ?, ?)';
+        db.query(query, [first, last, role, manager], (err, result) => {
+            if (err) throw err;
+            console.log('New employee added!');
+            start();
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 // AND update an employee role
 
+const updateEmployee = async () => {
+    db.query('SELECT last_name from employees', async (err, res) => {
+        try {
+            const { last_name } = await inquirer.prompt([
+                {
+                    name: 'last_name',
+                    type: 'list',
+                    message: 'Enter last name of employee you want to update',
+                    choices: res.map(({ last_name }) => last_name),
+                }
+            ]);
 
+            const { role_id } = await inquirer.prompt([
+                {
+                    name: 'role_id',
+                    type: 'list',
+                    message: 'Choose new role for employee',
+                    choiecs: [
+                        { name: 'Sales Lead', value: 1 },
+                        { name: 'Salesperson', value: 2 },
+                        { name: 'Lead Engineer', value: 3 },
+                        { name: 'Software Engineer', value: 4 },
+                        { name: 'Account Manager', value: 5 },
+                        { name: 'Accountant', value: 6 },
+                        { name: 'Legal Team Lead', value: 7 },
+                        { name: 'Lawyer', value: 8 },
+                    ]
+                }
+            ]);
+
+            const query = 'UPDATE employees SET role_id =? WHERE last_name =?';
+            db.query(query, [parseInt(role_id), last_name], (err, res) => {
+                if (err) throw err;
+                console.log('Updated employee!');
+            })
+        } catch (err) {
+            console.log(err);
+        }
+    })
+};
 
 // CHECK OFF TASKS WHEN DONE
 
